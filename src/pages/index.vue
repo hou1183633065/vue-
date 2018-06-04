bb383497aacd3f1691b96704e999ea0dbb149bdb
houzhiqiang <zhq.hou@muheda.com>
2018-06-04 14:26:39 +0800

    配置完成添加仓库

---
+++ src/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <p v-text="msg"></p>
+        <router-link to="home">linkto Home</router-link>
+    </div>
+</template>
+<script>
+export default {
+    data () {
+        return {
+            msg: 'hello index'
+        }
+    }
+}
+</script>
