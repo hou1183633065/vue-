28b79edd5e76b484d423de55ab490ff08519cfe6
houzhiqiang <zhq.hou@muheda.com>
2018-06-04 17:12:41 +0800

    添加vue-router，vuex

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,13 @@
 <template>
     <div>
+        <van-nav-bar
+        title="标题"
+        left-text="返回"
+        right-text="按钮"
+        left-arrow
+        @click-left="onClickLeft"
+        @click-right="onClickRight"
+        />
         <p v-text="msg"></p>
         <router-link to="home">linkto Home</router-link>
     </div>
@@ -9,6 +17,19 @@ export default {
     data () {
         return {
             msg: 'hello index'
+        }
+    },
+    methods: {
+        onClickLeft() {
+            this.$toast({
+                type: 'loading',
+                message: 'hello world',
+                mask: true,
+                duration: 0
+            });
+        },
+        onClickRight() {
+            this.$toast('按钮');
         }
     }
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Vant from 'vant'
+import 'vant/lib/vant-css/index.css'
 import index from '@/pages/index'
 import home from '@/pages/home'
 
 Vue.use(Router)
+Vue.use(Vant)
 export default new Router({
-    mode: 'history',
+    // mode: 'history',
     routes: [
         {
             path: '/',
